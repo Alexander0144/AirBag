0af92c496d1d7ea4719ab6bbefd79d869c071f21
Alexander0144 <56736324+Alexander0144@users.noreply.github.com>
2019-11-13 17:55:26 -0600

    Modificación de la vista principal y la vista de Carrito

---
+++ Airbag/Airbag.Usuario/Controllers/TransaccionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Airbag.Usuario.Controllers
+{
+    public class TransaccionController : Controller
+    {
+        // GET: Transaccion
+        public ActionResult Carrito()
+        {
+            return View();
+        }
+    }
+}
